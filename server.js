80d460ba62334919e0700c68a63b9e7d3820f762
Zack Gunther <56574067+19zgunther@users.noreply.github.com>
2022-04-27 11:52:47 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,66 @@
+const data = `
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gunther</title>
+</head>
+<body style='background-color: rgba(30,30,30);'>
+    <div style='color: white;'>
+        Hello! This is the ttle..
+    </div>
+    <script>
+        var ws = new WebSocket("ws://44.204.206.141:4000/");
+
+        ws.addEventListener("open", () => {
+                console.log("Connected - client");
+                ws.send("How are you?");
+        });
+        
+        ws.addEventListener('message', function (event) {
+                console.log(event.data);
+        });
+
+        setInterval(function() {
+                ws.send("My time is now");
+        }, 5000);
+
+    </script>
+</body>
+`
+
+const http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end(data);
+  console.log("Request made to server.");
+}).listen(3000);
+console.log('Server running on Amazon EC2');
+
+const websocket = require('ws');
+const wss = new websocket.Server({port:4000});
+
+wss.on("connection",  ws => {
+    console.log("new client connected");
+    // sending message
+    ws.on("message", data => {
+        console.log(`Client has sent us: ${data}`)
+    });
+    // handling what to do when clients disconnects from server
+    ws.on("close", () => {
+        console.log("the client has connected");
+            });
+    // handling client connection error
+    ws.onerror = function () {
+        console.log("Some Error occurred")
+    }
+});
+
+function sendMessage()
+{
+        ws.send("HI");
+}
+
+var interval = setInterval(sendMessage, 10000);
+
+        
